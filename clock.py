b59fe7e1e53b652d49bde9212dca7542f09de074
maheshdk4 <maheshdk46@gmail.com>
2025-09-14 12:33:24 +0530

    added digi clock lab

---
+++ clock.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from time import strftime
+
+def time():
+    current_time = strftime('%H:%M:%S')
+    label.config(text=current_time)
+    label.after(1000, time)  # update every second
+
+# Create window
+root = tk.Tk()
+root.title('Digital Clock')
+
+# Create label
+label = tk.Label(root, font=('Arial', 60), background='black', foreground='lime')
+label.pack(anchor='center')
+
+# Start the clock
+time()
+
+# Run the application
+root.mainloop()
